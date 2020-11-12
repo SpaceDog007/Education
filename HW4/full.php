8de3f3ca79228561adf3dad1c982163d76f136ad
Stanislav Ponomarev <sgponomarev@avito.ru>
2020-11-12 16:19:23 +0300

    HW4:gallery

---
+++ HW4/full.php
@@ -0,0 +1,9 @@
+<h1>Большое изображение</h1>
+<?php
+//$path = $_SERVER["DOCUMENT_ROOT"]."/user/4/gallery.php";
+//include_once($path);
+//
+//?>
+
+<img width="500" src="files/<?=$_GET['img']?>"><br>
+<a href="<?= $_SERVER['HTTP_REFERER']?>"> Назад </a>
